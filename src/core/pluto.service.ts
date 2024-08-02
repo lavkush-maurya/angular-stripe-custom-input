653403d6c783b92e21266803dfe2ef91252306f1
Lavv ⚝ <113632168+lavkush-maurya@users.noreply.github.com>
2024-08-02 06:56:46 +0000

    update live-backend url

--- src/core/pluto.service.ts
+++ src/core/pluto.service.ts
@@ -6,13 +6,12 @@ import { PaymentIntent } from '@stripe/stripe-js';
 
 export const PLUTO_ID = new InjectionToken<string>('[PLUTO] ClientID');
 
-// export const STRIPE_PUBLIC_KEY = 'pk_test_51Ii5RpH2XTJohkGafOSn3aoFFDjfCE4G9jmW48Byd8OS0u2707YHusT5PojHOwWAys9HbvNylw7qDk0KkMZomdG600TJYNYj20';
 export const STRIPE_PUBLIC_KEY = 'pk_test_51Php9jJ4tzySvsYcvWsEsxQ1CbeTR34weX83xPDsxVucVS1BaSJ9pkDn9JrKHgFwI0He9M2KbcZXcWnCbDWE28fG00tYfPDxo7';
 
 @Injectable({ providedIn: 'root' })
 export class PlutoService {
-  // private static readonly BASE_URL = 'https://api.pluto.ricardosanchez.dev/api';
-  private static readonly BASE_URL = 'http://localhost:3000/api';
+  private static readonly BASE_URL = 'https://backend-stripe-4q2a.onrender.com/api'; //live
+  // private static readonly BASE_URL = 'http://localhost:3000/api'; // local
 
 
   constructor(
